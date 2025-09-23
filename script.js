0326ecbd0af3e1c4805619fa769eae9fa76362cd
Sohidur Rahman <sorahman@redhat.com>
2025-09-23 18:25:21 -0400

    Initial commit - Seekers Hive landing page

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("clickMe").addEventListener("click", () => {
+    const msg = document.getElementById("message");
+    msg.innerText = "👋 Hello from Seekers Hive!";
+    msg.style.fontWeight = "bold";
+    msg.style.color = "#ff4e50";
+  });
+  
